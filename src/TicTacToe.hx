bccf25358bbf294e3dbe3a58ebe3b5a1bd1f8021
Sid <saytleader@outlook.com>
2023-10-20 10:28:26 -0400

    Create TicTacToe.hx

---
+++ src/TicTacToe.hx
@@ -0,0 +1,12 @@
+package;
+
+class TicTacToe {
+    public static function play() {
+        try {
+            
+        } catch (e:Eof) {
+            Sys.print("\033[2J");
+            Main.prompt();
+        }
+    }
+}
