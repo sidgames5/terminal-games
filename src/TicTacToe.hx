c14045944c5b2a4544d5c37213a0cce3b97faa52
Sid <saytleader@outlook.com>
2023-11-08 08:30:15 -0500

    Update TicTacToe.hx

--- src/TicTacToe.hx
+++ src/TicTacToe.hx
@@ -1,10 +1,12 @@
 package;
 
 class TicTacToe {
-    static var game = [["", "", ""],["", "", ""],["", "", ""]];
+    static var game = [[" ", " ", " "],[" ", " ", " "],[" ", " ", " "]];
     public static function play() {
         try {
-            
+            printGrid();
+            Sys.print("Enter coordinates: ");
+            final coords = Sys.stdin().readLine().split("");
         } catch (e:Eof) {
             Sys.print("\033[2J");
             Main.prompt();
@@ -12,8 +14,9 @@ class TicTacToe {
     }
 
     static function printGrid() {
-        Sys.println('┏━');
-        Sys.println('┃');
-        Sys.println('');
+        Sys.println(" 123");
+        Sys.println("A" + game[0].join(""));
+        Sys.println("B" + game[1].join(""));
+        Sys.println("C" + game[2].join(""));
     }
 }
